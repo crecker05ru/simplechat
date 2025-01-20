293ae4fdebe3a3d6956695aacbcbf912d0927fa1
Anvar <fort2652@rambler.ru>
2025-01-20 19:24:06 +0300

    https: add https

--- expressWs.js
+++ expressWs.js
@@ -6,6 +6,8 @@ const fs = require("fs");
 const websocketController = require("./websocket/websocketController");
 const expressWsModule = require("./websocket/expressWsModule");
 const wsRouter = "./routes/wsRouter";
+const https = require('https')
+const { WebSocket } = require('ws')
 var express = require("express");
 const { send } = require("process");
 const app = express();
@@ -64,7 +66,32 @@ app.ws("/echo", (ws, req) => {
 
 const start = async () => {
   try {
+    if(process.env.WEBSOCKET_KEY && process.env.WEBSOCKET_CERT){
+      const key = fs.readFileSync(process.env.WEBSOCKET_KEY);
+      const cert = fs.readFileSync(process.env.WEBSOCKET_CERT);
+      const server = https.createServer({ key, cert }, express)
+      const wss = new WebSocket.Server({ server });
+
+      wss.on('connection', ws => {
+        console.log('Client connected.');
+        ws.send('Hi there!');
+      })
+      wss.on('message', msg => {
+        console.log('Client said: ' + msg.toString()); 
+      });
+    // app.listen(PORT, () => console.log(`Server started on port ${PORT} WITH CERT`));
+
+    server.listen(PORT, err => {
+      if (err) {
+        console.log('Well, this didn\'t work...');
+        process.exit();
+      }
+      console.log('Server is listening on port ' + PORT);
+    });
+
+    }else {
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    }
   } catch (e) {
     console.log(e);
   }
